23b85887dbd8e211e979b12c341246efeb94c931
Kohei Kumazaki <kumazaki98@gmail.com>
2019-03-30 02:20:24 +0900

    ABC122 C still slow

--- Contest/ABC122/C.cpp
+++ Contest/ABC122/C.cpp
@@ -19,6 +19,26 @@ int main () {
         cin >> l >> r;
         // string target = str.substr(l-1, r-1);
         int index = 0;
+        int cursor = l-1;
+        while (cursor < r) {
+            int step = 1;
+            if (str[cursor] == 'G' || str[cursor] == 'T') {
+                step = 2;
+            }
+            else if (str[cursor] == 'C' ) {
+                if ( cursor != l-1 && str[cursor-1] == 'A') {
+                    nACNum++;
+                }
+                step = 2;
+            }
+            else {
+                step = 1;
+            }
+
+            cursor += step;
+        }
+
+#if 0
         for (int j = l-1; j < r; ++j) {
             char s = str[j];
             if (s == AC[index]) {
@@ -34,7 +54,7 @@ int main () {
                 index = 0;
             }
         }
-
+#endif
         cout << nACNum << endl;
 
     }
