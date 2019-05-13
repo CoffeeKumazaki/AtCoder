e50c6ac8af94ae2e86297f88e11eadd25f4bd29d
Kohei Kumazaki <kumazaki98@gmail.com>
2019-05-13 23:08:36 +0900

    add ABC120 C

--- Contest/ABC120/C.cpp
+++ Contest/ABC120/C.cpp
@@ -9,7 +9,19 @@
 
 using namespace std;
 int main(int argc, const char * argv[]) {
-    
+
+    string s;
+    cin >> s;
+    int z = 0;
+    for (int i=0; i< s.size(); i++) {
+        if (s[i] == '0') {
+            z ++;
+        }
+    }
+
+    z = min(z, (int)s.size()-z);
+
+    cout << z*2 << endl;
     
     return 0;
 }
