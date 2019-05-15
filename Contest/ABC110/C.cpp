141f96d3eb43e84b31b7d4d5ff848728408603a0
Kohei Kumazaki <kumazaki98@gmail.com>
2019-05-15 23:41:02 +0900

    add ABC110-C

--- Contest/ABC110/C.cpp
+++ Contest/ABC110/C.cpp
@@ -6,10 +6,50 @@
 #include <functional>
 #include <cmath>
 #include <string>
+#include <map>
 
 using namespace std;
+
+pair<char, int> max_kay(map<char, int>& m) {
+  int max = 0;
+  char key = m.begin()->first;
+  for (map<char, int>::iterator it = m.begin(); it != m.end(); it++) {
+    if (max < it->second) {
+        max = it->second;
+        key = it->first;
+    }
+  }
+  return make_pair(key, max);
+}
+
 int main(int argc, const char * argv[]) {
     
-    
+    string s, t;
+    cin >> s >> t;
+
+    map<char, int> ms, mt;
+
+    for (int i=0; i<s.size(); i++) {
+        ms[s[i]]++;
+        mt[t[i]]++;
+    }
+
+    bool b = true;
+
+    for (int i=0; i<ms.size(); i++){
+        
+        pair<char, int> sp = max_kay(ms);
+        pair<char, int> tp = max_kay(mt);
+        if (sp.second != tp.second) {
+            b = false;
+            break;
+        }
+        ms[sp.first] = 0;
+        mt[tp.first] = 0;
+    }
+
+    cout << ( b ? "Yes" : "No" ) <<  endl;
+
+
     return 0;
 }
