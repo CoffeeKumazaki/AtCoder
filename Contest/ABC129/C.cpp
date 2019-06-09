6f9dcd9384959cda7bc163c7468d963eb9f3c858
Kohei Kumazaki <kumazaki98@gmail.com>
2019-06-09 23:37:47 +0900

    AC ABC129 C

--- Contest/ABC129/C.cpp
+++ Contest/ABC129/C.cpp
@@ -15,32 +15,24 @@ int main(int argc, const char * argv[]) {
     cin >> n >> m;
     n++;
     vector<int> a(n,0);
-    vector<long> s(n,0);
+    vector<int> s(n,0);
     for(int i=0; i<m; i++) {
         int t;
         cin >> t;
         a[t] = 1;
     }
 
-    long t = 1;
-    int id=0;
-    for (int i=0; i<n; i++) {
-        if (i-id <= 1) {
-            s[i] = a[i] ? 0 : 1;
+    s[0] = a[0] ? 0 : 1;
+    s[1] = a[1] ? 0 : 1;
+    
+    for (int i=2; i<n; i++) {
+        if (!a[i]) {
+            s[i] = s[i-1] + s[i-2];
+            s[i] %=  1000000007;
         }
-        else {
-            s[i] = a[i] ? 0 : s[i-1] + s[i-2];
-        }
-
-        if (a[i])  {
-            t *= s[i-1];
-            id = i+1;
-        }
-        
-        // cout << a[i] << " " << s[i] << endl;
     }
 
-    cout << (t*s[n-1])%1000000007 << endl;
+    cout << s[n-1] << endl;
 
     return 0;
 }
