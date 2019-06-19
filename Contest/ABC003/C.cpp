701a018f02b99e658305486ed551ae41b5f77671
Kohei Kumazaki <kumazaki98@gmail.com>
2019-06-19 22:17:24 +0900

    add ABC003-C

--- Contest/ABC003/C.cpp
+++ Contest/ABC003/C.cpp
@@ -11,6 +11,20 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    
+    int n, k;
+    cin >> n >> k;
+    vector<double> r(n);
+    for (int i=0; i<n; i++) {
+        cin >> r[i];
+    }
+
+    sort(r.begin(), r.end());
+
+    double s=0.0;
+    for (int i = n-k; i < n; i++)
+    {
+        s = (s+r[i])/ 2.0;
+    }
+    printf("%lf\n", s);
     return 0;
 }
